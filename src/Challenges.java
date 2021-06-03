a265774314c57d6faa9f8b96661a134699e22fea
dgreen244 <deandregreen99@gmail.com>
2021-06-03 13:46:19 -0500

    all solutions

--- src/Challenges.java
+++ src/Challenges.java
@@ -1,16 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Challenges {
     /** Create a function which returns the number of true values there are in an array.*/
+
+    //working
     public static int countTrueFalse(boolean[] arr) {
-        // write your code here
-        // you will need to change the return statement
-        return Integer.parseInt(null);
+      /*set up a statement that counts the amount of times that
+       true appears in the arraylist.
+       I could use a for loop that is set up to use an int++ for everytime that "True" is present*/
+        int count = 0;
+        for( boolean each:arr ){
+            if(each) count++;
+        }
+        return count;
     }
 
     /** Create a function that takes an array and returns the absolute difference between the biggest and smallest numbers.*/
+
+    //working
     public static int differenceMinMax(int[] nums){
-        // write your code here
-        // you will need to change the return statement
-        return Integer.parseInt(null);
+        /*write a function that takes the biggest and smallest numbers in an arraylist and find the absolute value
+        between the two of them*/
+            Arrays.sort(nums);
+            return Math.abs(nums[0]-nums[nums.length-1]);
+
     }
 
     /**
@@ -19,17 +33,23 @@ public class Challenges {
      * The first two letters are repeated twice with an ellipsis ... and space after each,
      * and then the word is pronounced with a question mark ?
      */
+
+    //working
     public static String stutter(String str){
-        // write your code here
-        // you will need to change the return statement
-        return "";
+        String ellipsis = "... ";
+        String result = "" + str.charAt(0) + str.charAt(1) + ellipsis;
+        result = result + result;
+
+        return result + str + "?";
     }
 
     /** Write a function that takes an array of numbers and returns the second largest number.*/
+
+    //working
     public static int secondLargestNumber(int[] nums){
-        // write your code here
-        // you will need to change the return statement
-        return Integer.parseInt(null);
+        Arrays.sort(nums);
+        return nums[nums.length-2];
+
     }
 
     /**
@@ -41,10 +61,23 @@ public class Challenges {
      If the number is not a multiple of either 3 or 5, the number should be output on its own as shown in the examples below.
      The output should always be a string even if it is not a multiple of 3 or 5.
      **/
+
+    //working
     public static String fizzBuzz(int num){
-        // write your code here
-        // you will need to change the return statement
-        return "";
+        String output = Integer.toString(num);
+        if ((num % 3 ==0) && (num % 5==0)){
+            output = "FizzBuzz";
+            System.out.println("FizzBuzz");
+        }
+        else if (num % 3 == 0){
+           output = "Fizz";
+                   System.out.println("Fizz");
+        }
+        else if (num % 5 == 0){
+            output = "Buzz";
+            System.out.println("Buzz");
+        }
+        return output;
     }
 
     /**
@@ -52,48 +85,90 @@ public class Challenges {
      * Write a program that gets three integers from the user.
      * Count from the first number to the second number in increments of the third number.
      */
-    public static int[] skipCount(int countFrom, int countTo, int countBy){
-        // write your code here
-        // you will need to change the return statement
-        return new int[0];
+
+    //working
+    public static String skipCount(int countFrom, int countTo, int countBy){
+        // Validate
+        if(countTo < countFrom){
+            return "Try again with better numbers!";
+        }
+        // hold the pattern temporarily
+        StringBuilder answer= new StringBuilder();
+        // create Pattern based on user input, using a for loop to create the pattern
+        for (int currentInt=countFrom; currentInt<=countTo; currentInt += countBy){
+            answer.append(currentInt).append(".. ");
+        }
+        // remove the last three characters because they are extras
+        answer.setLength(answer.length()-3);
+        //return our answer
+        return answer.toString();
     }
 
     /** The "Reverser"
      * takes a string as input and returns that string in reverse order, with the opposite case.*/
+
+    //working
     public static String reverser(String reverseMe){
-        // write your code here
+        String result = "";
+        for(char c: reverseMe.toCharArray()){
+            if (Character.isUpperCase(c)){
+                c = Character.toLowerCase(c);
+            } else {
+                c = Character.toUpperCase(c);
+            }
+            result = c + result;
+        }
+
         // you will need to change the return statement
-        return "";
+        return result;
     }
 
     /**
      * Reverse An Array
      * Write a function that reverses the order of an array
      */
+
+    //working
     public static int[] reverseArray(int[] arr){
-        // write your code here
+        int arrlength = arr.length;
+        int[] newarr = new int[arr.length];
+        for(int x=0; x< arrlength; x++){
+            int pos = arrlength-x-1;
+            //System.out.println(x+"      "+pos);
+            //System.out.println(arr[x]+"      "+arr[pos]);
+            newarr[x] = arr[pos];
+            //System.out.println(newarr[x]+"      "+newarr[pos]);
+        }
         // you will need to change the return statement
-        return new int[0];
+        return newarr;
     }
 
     /**
      * Write a function that finds the word "bomb" in the given string (not case sensitive).
      * Return "DUCK!" if found, otherwise,"Relax, there's no bomb.".
      */
+
+    //working
     public static String duckTheBomb(String checkMe){
-        // write your code here
-        // you will need to change the return statement
-        return "";
+        //Search string for "bomb"
+        //if bomb is found return "DUCK!"
+        checkMe = checkMe.toLowerCase();
+        if (checkMe.contains("bomb")){
+            return ("DUCK!");
+        }
+        //if bomb is not found return "Relax, there's no bomb.".
+        return "Relax, there's no bomb.";
     }
 
     /** Sort in Ascending Order
      * Create a method that takes an array of integers and returns a new array, sorted in ascending order (smallest to biggest).
      * If the function's argument is null, an empty array or undefined, return an empty array
      */
+
+    //working
     public static int[] sortNumsAscending(int[] nums){
-        // write your code here
-        // you will need to change the return statement
-        return new int[0];
+        Arrays.sort(nums);
+        return nums;
     }
 
     /**
@@ -101,9 +176,15 @@ public class Challenges {
      * Create a method that takes an array of strings,removes all duplicate items
      * and returns a new array in the same sequential order as the old array (minus duplicates).
      */
+
+    //working
     public static String[] removeDups(String[] str){
-        // write your code here
-        // you will need to change the return statement
-        return new String[0];
+        ArrayList<String> cleanList = new ArrayList<>();
+        for(String each : str){
+            if(!cleanList.contains(each)){
+                cleanList.add(each);
+            }
+        }
+        return cleanList.toArray(new String[0]);
     }
 }
--- src/CountTrue_Test.java
+++ src/CountTrue_Test.java
@@ -5,7 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CountTrue_Test {
     @Test
     public void countTrue_1() {
-        assertEquals(Challenges.countTrueFalse(new boolean[] { true, false, false, true, false }), (2));
+        boolean[] test =new boolean[] { true, false, false, true, false };
+        assertEquals(Challenges.countTrueFalse(test), (2));
     }
 
     @Test
